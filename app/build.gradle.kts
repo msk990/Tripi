03e037e6237f61cdc48e15df37e31bfc8c5d3acb
juje2020 <msk990@yahoo.com>
2025-05-27 21:28:51 +0300

    fix nav icons v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
-    implementation(libs.mlkit.object.detection)
+    implementation("com.google.mlkit:object-detection:17.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
